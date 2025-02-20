6c7107d3599e875a7b533309f8dade3df17333c5
aprianwdh <aprianwidhiwibowo@gmail.com>
2025-02-20 19:38:25 +0700

    20-2-25

--- Prorotipe1/Assets/Scripts/Enemy/Enemy.cs
+++ Prorotipe1/Assets/Scripts/Enemy/Enemy.cs
@@ -1,22 +1,32 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Enemy : MonoBehaviour
 {
     public int maxHealth = 3;
     public float speed = 5.0f;
     public float chaseRange = 10.0f;
+    public Transform Guardhouse;
 
 
     private int currentHealth;
     private Rigidbody2D rb;
     private bool isChase = false;
     private Transform Targer;
+    private Animator anim;
+    private bool isWalk = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentHealth = maxHealth;
         rb = GetComponent<Rigidbody2D>();
         Targer = GameObject.FindGameObjectWithTag("Player").transform;
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        AnimationControler();
     }
 
     private void FixedUpdate()
@@ -26,27 +36,66 @@ public class Enemy : MonoBehaviour
 
     void ChasePlayer()
     {
+        //jika player berada dalam jarak chase
         if (Vector2.Distance(transform.position, Targer.position) < chaseRange)
         {
             isChase = true;
-            Debug.Log("Chase");
-
+            isWalk = true;
             if (Targer.position.x > transform.position.x)
             {
                 rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
-                transform.eulerAngles = new Vector3(0, 0, 0);
+                transform.eulerAngles = new Vector3(0, 180, 0);
             }
             else
             {
                 rb.linearVelocity = new Vector2(-speed, rb.linearVelocity.y);
-                transform.eulerAngles = new Vector3(0, 180, 0);
+                transform.eulerAngles = new Vector3(0, 0, 0);
             }
         }
+        //jika player diluar jangkauan chase
         else
         {
             isChase = false;
-            Debug.Log("Not Chase");
+            backToGuardHouse();
         }
+    }
+
+    void backToGuardHouse()
+    {
+        if (!isChase)
+        {
+            float distanceToGuardhouse = Vector2.Distance(transform.position, Guardhouse.position);
+
+            // Jika belum sampai di Guardhouse
+            if (distanceToGuardhouse > 0.1f)
+            {
+                Vector2 directionToGuardhouse = (Guardhouse.position - transform.position).normalized;
+                rb.linearVelocity = directionToGuardhouse * speed;
+
+                // Menyesuaikan arah musuh
+                if (Guardhouse.position.x > transform.position.x)
+                {
+                    transform.eulerAngles = new Vector3(0, 180, 0);
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                }
+                isWalk = true;
+            }
+            else
+            {
+                // Jika sudah sampai, berhenti
+                rb.linearVelocity = Vector2.zero;
+                isWalk = false;
+                Debug.Log("Sudah sampai di Guardhouse");
+            }
+        }
+    }
+
+    void AnimationControler()
+    {
+        anim.SetBool("isChase", isWalk);
     }
 
     public void TakeDamage(int damage)
@@ -73,6 +122,7 @@ public class Enemy : MonoBehaviour
     {
         Destroy(gameObject);
     }
+
 
     private void OnDrawGizmos()
     {
